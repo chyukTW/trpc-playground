120e4c23e5b39e58d2092ea286a2207d6e58b8b2
이창혁 <ch22@twinny.co.kr>
2023-12-01 16:51:39 +0900

    add example

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,36 @@
+import { trpc } from "./trpc";
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { httpLink } from '@trpc/client';
+import { useState } from 'react';
+
+const Hello = () => {
+  const response = trpc.hi.useQuery({name: 'twinny'});
+
+  return <p>{response.data?.text}</p>
+}
+
 function App() {
+  const [queryClient] = useState(() => new QueryClient());
+
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpLink({
+          url: 'http://localhost:2022',
+        }),
+      ],
+    }),
+  );
+
   return (
-    <div>client</div>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <Hello />
+      </QueryClientProvider>
+    </trpc.Provider>   
   );
 }
+
 
 export default App;

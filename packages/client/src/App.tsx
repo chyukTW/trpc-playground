ae1ef2de90ad6506e1ed50cdba2c5139d2549c2e
chyukTW <ch22@twinny.co.kr>
2023-12-02 01:47:52 +0900

    simple wss

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,24 +1,47 @@
-import { trpc } from "./trpc";
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { httpLink } from '@trpc/client';
+import { createWSClient, httpLink, splitLink, wsLink } from '@trpc/client';
 import { useState } from 'react';
+
+import { trpc } from './trpc';
 
 const Hello = () => {
   const response = trpc.hello.useQuery({name: 'twinny'});
 
-  return <p>{response.data?.text}</p>
-}
+  return <p>{response.data?.text}</p>;
+};
+
+const Number = () => {
+  const [number, setNumber] = useState<number>();
+
+  trpc.randomNumber.useSubscription(undefined, {
+    onData: ({randomNumber})=> {
+      setNumber(randomNumber);
+    }
+  });
+
+  return <p>{number}</p>;
+};
 
 function App() {
   const [queryClient] = useState(() => new QueryClient());
 
   const [trpcClient] = useState(() =>
     trpc.createClient({
-      links: [
-        httpLink({
-          url: 'http://localhost:2022',
+      links: [  
+        splitLink({
+          condition(op) {
+            return op.type === 'subscription';
+          },
+          false: httpLink({
+            url: 'http://localhost:2022',
+          }),
+          true: wsLink({
+            client: createWSClient({
+              url: 'ws://localhost:2022',
+            })
+          })
         }),
+        
       ],
     }),
   );
@@ -27,6 +50,7 @@ function App() {
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
         <Hello />
+        <Number />
       </QueryClientProvider>
     </trpc.Provider>   
   );
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,26 +1,58 @@
 import { initTRPC } from '@trpc/server';
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
+import { applyWSSHandler } from '@trpc/server/adapters/ws';
+import { observable } from '@trpc/server/observable';
 import cors from 'cors';
+import { WebSocketServer } from 'ws';
 import { z } from 'zod';
 
 export type AppRouter = typeof appRouter;
 
-const t = initTRPC.create();
+type Context = Awaited<ReturnType<typeof createContext>>;
 
+// init
+
+const t = initTRPC.context<Context>().create();
 const { procedure, router } = t;
 
+// router
 const appRouter = router({
   hello: procedure.input(z.object({ name: z.string() })).query(({ input }) => {
     return {
       text: `Hello ${input.name}`,
     };
   }),
+  randomNumber: procedure.subscription(() => {
+    return observable<{ randomNumber: number }>((emit) => {
+      const timer = setInterval(() => {
+        emit.next({ randomNumber: Math.random() });
+      }, 700);
+
+      return () => {
+        clearInterval(timer);
+      };
+    });
+  }),
 });
 
-const { listen } = createHTTPServer({
-  createContext: () => (console.log('context!'), {}),
+// context
+const createContext = () => {
+  console.log('context !');
+
+  return {};
+};
+
+// http
+const { listen, server } = createHTTPServer({
+  createContext,
   middleware: cors(),
   router: appRouter,
 });
 
+// wss
+const wss = new WebSocketServer({ server });
+
+applyWSSHandler<AppRouter>({ createContext, router: appRouter, wss });
+
+// run
 listen(2022);

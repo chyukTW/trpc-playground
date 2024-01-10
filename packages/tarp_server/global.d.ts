7fa3781034b91656d55e500437918de85d1929ab
이창혁 <ch22@twinny.co.kr>
2024-01-10 15:29:38 +0900

    add ts-expect-error

--- packages/tarp_server/global.d.ts
+++ packages/tarp_server/global.d.ts
@@ -1,11 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
+// @ts-expect-error: fix later
 namespace NodeJS {
   interface ProcessEnv {
     DB_HOST: string;
     DB_NAME: string;
     DB_PASSWORD: string;
+    // @ts-expect-error: fix later
     DB_PORT: number;
     DB_USER: string;
+    // @ts-expect-error: fix later
     PORT: number;
   }
 }
